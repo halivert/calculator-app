b8abd754ebe4499b6dc04137be229f335f0bf2b4
Halí V <halivelazquez@outlook.com>
2021-06-14 13:12:56 -0500

    Update theme selector, use localStorage

--- js/theme-selector.js
+++ js/theme-selector.js
@@ -1,46 +1,64 @@
+const appName = "calculator-app";
 const darkTheme = window.matchMedia("(prefers-color-scheme: dark)");
 const lightTheme = window.matchMedia("(prefers-color-scheme: light)");
 
-function setTheme(theme) {
+function setTheme(theme, writeStorage) {
 	const rootClassList = document.documentElement.classList;
 	const themeSwitcher = document.getElementById("themeSwitcher");
+
+	if (writeStorage) {
+		localStorage.setItem(appName, JSON.stringify({ theme: theme }));
+	}
 
 	if (theme === "dark") {
 		rootClassList.remove("light-theme", "purple-theme");
 		rootClassList.add("dark-theme");
 		if (themeSwitcher) themeSwitcher.value = 1;
-		return;
 	}
 
 	if (theme === "light") {
 		rootClassList.remove("dark-theme", "purple-theme");
 		rootClassList.add("light-theme");
 		if (themeSwitcher) themeSwitcher.value = 2;
-		return;
 	}
 
-	rootClassList.remove("dark-theme", "light-theme");
-	rootClassList.add("purple-theme");
+	if (theme === "purple") {
+		rootClassList.remove("dark-theme", "light-theme");
+		rootClassList.add("purple-theme");
+		if (themeSwitcher) themeSwitcher.value = 3;
+	}
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-	if (lightTheme.matches) {
-		themeSwitcher.value = 2;
+	const className = document.documentElement.classList[0].split("-")[0];
+
+	if (className === "dark") {
+		themeSwitcher.value = "1";
+	} else if (className === "light") {
+		themeSwitcher.value = "2";
+	} else if (className === "purple") {
+		themeSwitcher.value = "3";
 	}
 
 	themeSwitcher.addEventListener("change", ({ target }) => {
-		if (target.value === "1") setTheme("dark");
-		else if (target.value === "2") setTheme("light");
-		else setTheme("purple");
+		if (target.value === "1") setTheme("dark", true);
+		else if (target.value === "2") setTheme("light", true);
+		else setTheme("purple", true);
 	});
 });
 
-setTheme(darkTheme.matches ? "dark" : "light");
-
 darkTheme.addEventListener("change", (evt) => {
-	if (evt.matches) setTheme("dark");
+	if (evt.matches) setTheme("dark", false);
 });
 
 lightTheme.addEventListener("change", (evt) => {
-	if (evt.matches) setTheme("light");
+	if (evt.matches) setTheme("light", false);
 });
+
+let storage = JSON.parse(localStorage.getItem(appName));
+
+if (storage) {
+	setTheme(storage.theme || "dark", false);
+} else {
+	setTheme(darkTheme.matches ? "dark" : "light");
+}
