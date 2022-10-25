5ba3329fcace8144292259ff61522eff85d697dc
David Puente <davidpuent@gmail.com>
2022-10-26 00:57:39 +0200

    revert(root): :bug: revert #3 due to unexpected operation that breaks in operation

--- js/index.js
+++ js/index.js
@@ -23,57 +23,47 @@ const operations = {
 const screen = document.querySelector('.screen > span')
 
 const addButtonsListeners = () => {
-  document.querySelectorAll('.keypad > button').forEach((btn) => {
+  document.querySelectorAll('.keypad > button').forEach(btn => {
     buttons[btn.id] = btn
 
     btn.addEventListener('click', handleButtonClick)
   })
 }
 
-const handleButtonClick = (e) => {
+const handleButtonClick = e => {
   const { id } = e.target
   !isNaN(id) || id === '.' ? handleNumberClick(id) : handleOperatorClick(id)
 }
 
-const handleNumberClick = (number) => {
+const handleNumberClick = number => {
   const screenValue = screen.textContent
 
   if (number === '.' && currentOperation.displayValue.includes('.')) return
 
-  currentOperation.displayValue =
-    currentOperation.displayValue === '0' ||
-    (currentOperation.operator !== null && currentOperation.secondNumber === 0)
-      ? number
-      : screenValue + number
+  currentOperation.displayValue = currentOperation.displayValue === '0' || (currentOperation.operator !== null && currentOperation.secondNumber === 0)
+    ? number
+    : screenValue + number
 
   if (currentOperation.operator === null) {
-    currentOperation.firstNumber = Number(
-      currentOperation.displayValue.replace(/\,/g, '')
-    )
+    currentOperation.firstNumber = Number(currentOperation.displayValue)
   }
 
   if (currentOperation.operator !== null) {
-    currentOperation.secondNumber = Number(
-      currentOperation.displayValue.replace(/\,/g, '')
-    )
+    currentOperation.secondNumber = Number(currentOperation.displayValue)
   }
 
   updateScreen()
 }
 
-const handleOperatorClick = (operator) => {
+const handleOperatorClick = operator => {
   if (operator === 'reset') {
     operations[operator]()
     return
   }
 
   if (operator === 'del') {
-    currentOperation.displayValue = operations.del(
-      currentOperation.displayValue
-    )
-    currentOperation.operator === null
-      ? (currentOperation.firstNumber = Number(currentOperation.displayValue))
-      : (currentOperation.secondNumber = Number(currentOperation.displayValue))
+    currentOperation.displayValue = operations.del(currentOperation.displayValue)
+    currentOperation.operator === null ? currentOperation.firstNumber = Number(currentOperation.displayValue) : currentOperation.secondNumber = Number(currentOperation.displayValue)
     updateScreen()
     return
   }
@@ -81,22 +71,15 @@ const handleOperatorClick = (operator) => {
   if (currentOperation.operator === null) {
     currentOperation.operator = operator
     currentOperation.secondNumber = 0
-    if (
-      currentOperation.displayValue !== currentOperation.firstNumber.toString()
-    ) {
-      currentOperation.firstNumber = Number(
-        currentOperation.displayValue.replace(/\,/g, '')
-      )
+    if (currentOperation.displayValue !== currentOperation.firstNumber.toString()) {
+      currentOperation.firstNumber = Number(currentOperation.displayValue)
     }
     updateScreen()
     return
   }
 
   if (operator === 'eq') {
-    currentOperation.displayValue = operations[currentOperation.operator](
-      currentOperation.firstNumber,
-      currentOperation.secondNumber
-    ).toLocaleString()
+    currentOperation.displayValue = operations[currentOperation.operator](currentOperation.firstNumber, currentOperation.secondNumber).toString()
     currentOperation.operator = null
     currentOperation.firstNumber = 0
     currentOperation.secondNumber = null
@@ -104,10 +87,7 @@ const handleOperatorClick = (operator) => {
     return
   }
 
-  currentOperation.firstNumber = operations[currentOperation.operator](
-    currentOperation.firstNumber,
-    currentOperation.secondNumber
-  )
+  currentOperation.firstNumber = operations[currentOperation.operator](currentOperation.firstNumber, currentOperation.secondNumber)
   currentOperation.operator = operator
   currentOperation.secondNumber = 0
   currentOperation.displayValue = currentOperation.firstNumber.toString()
@@ -115,14 +95,7 @@ const handleOperatorClick = (operator) => {
 }
 
 const updateScreen = () => {
-  const num = currentOperation.displayValue.split('.')
-  let whole = num[0]
-  whole = whole.replace(/\,/g, '')
-  let fractional = ''
-  if (num.length > 1) {
-    fractional = '.' + num[1]
-  }
-  screen.textContent = (+whole).toLocaleString() + fractional
+  screen.textContent = currentOperation.displayValue
 }
 
 const init = () => {
